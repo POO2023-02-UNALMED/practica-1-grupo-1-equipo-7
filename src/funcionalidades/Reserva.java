11b7abfd02363e2cb80fc1ab4a7a54d968589e08
LuaN1409 <141962309+LuaN1409@users.noreply.github.com>
2023-10-09 02:32:23 -0500

    Update Reserva.java

--- src/funcionalidades/Reserva.java
+++ src/funcionalidades/Reserva.java
@@ -1,8 +1,40 @@
 package funcionalidades;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
+import clases.Sedes;
 
 public class Reserva {
 
+	static Scanner sc = new Scanner(System.in);
+	static ArrayList <Sedes> sedes = new ArrayList<Sedes>();
+	static Scanner in = new Scanner(System.in);
+
 	
+	
+public static void main(String[] args) {
+	int opcion = 0;
+	boolean Salir;
+	
+	do {
+		System.out.println("\n\nMenú Principal");
+		System.out.println("\n\3.Reservaciones");
+		System.out.println("\n\nOpcion 1 - Reservar");
+		System.out.println("\n\nOpcion 2 - Cancelar o Modificar Reservación");
+		opcion = in.nextInt();
+		
+		switch(opcion) {
+		case 1:
+			ReservarMesa();
+			break;
+		}
+		
+		
+		
+	}
+	
+
+	
+}
+
+
 }
