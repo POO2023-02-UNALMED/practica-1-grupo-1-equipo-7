c3b6bc41cd92d70ecd0011cf6977b0a36e19f160
otaku9820 <aguido@unal.edu.co>
2023-10-10 11:49:21 -0500

    g

--- src/clases/Inventario.java
+++ src/clases/Inventario.java
@@ -2,16 +2,19 @@ package clases;
 import java.util.List;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
-import java.time.LocalDate;
+
 
 
 public class Inventario  {
 	
-	private static  int cantidadTotal;
+	
 	
 	private Map <Item,Integer > listadoItems= new HashMap<>();
+	private List<Item>listadoVencidos=new ArrayList<>();
+	private static  int cantidadTotal;
 	
 	
 
@@ -21,15 +24,15 @@ public class Inventario  {
 //	constructor 
 	public Inventario(  Item item) {
        
-        cantidadTotal++;
-//   Añadir diccionario, con su valor y clave si la clave se repite, el valor aumenta en uno
+     // Añadir diccionario, con su valor y clave si la clave se repite, el valor aumenta en uno
         
         int valorActual = listadoItems.get(item);
         if (!listadoItems.containsKey(item)) {
             listadoItems.put(item, 0);
         } else {
             
-            listadoItems.put(item, valorActual ++);}
+            listadoItems.put(item, valorActual +=item.getCantidad());}
+        cantidadTotal++;
         }
         
 
@@ -65,13 +68,14 @@ public class Inventario  {
     
     
     	
-    
+    // retirar items del inventario 
     public void retirarItems(Item item, int cantidad) {
         if (listadoItems.containsKey(item)) {
             int cantidadActual = listadoItems.get(item);
             if (cantidadActual >= cantidad) {
                 listadoItems.put(item, cantidadActual - cantidad);
                 cantidadTotal -= cantidad; // Actualizar la cantidad total en el inventario
+                
             } else {
                 // Manejar el caso en el que intentas retirar más de lo que hay disponible.
                 System.out.println("No hay suficientes " + item.getNombre() + " en el inventario.");
@@ -87,22 +91,63 @@ public class Inventario  {
              System.out.println(clave + ": " + valor);
          }
      }
-    public void revisionNivelesStock(String fecha ) {
-    	List <Item>listadoVencidos=new ArrayList<>();
-    	for (Item objetos: listadoItems.keySet()) {
-    		LocalDate fechaVencimiento=LocalDate.parse(objetos.getFechaVencimiento());
-    		
-    		LocalDate  fechaActual=LocalDate.parse(fecha);
-    		if(fechaActual.compareTo(fechaActual)>0) {
-    			listadoVencidos.add(objetos);
-    			
-    			
-    		}
-    		}
+    public void añadirProductosVencidos(Date fecha ) {
     	
+    	
+    	
+    	for (Item i:Item.getListadoItems()) {
+    		Date fechaVencimiento=i.getFechaVencimiento();
+    		if (fechaVencimiento.compareTo(fecha)<0) {
+    			listadoVencidos.add(i);
+    			
+    		}
+    		
+    		
+    	}
+    	}
+    public  void imprimirProductosVencidos() {
+        Map<String, Integer> productoCantidadMap = new HashMap<>();
+
+        // Iterar sobre la lista de productos vencidos
+        for (Item producto : this.listadoVencidos) {
+            String nombre = producto.getNombre();
+            Integer cantidad = producto.getCantidad();
+            Integer cantidadActual=productoCantidadMap.get(nombre);
+
+            // Si el producto ya existe en el mapa, sumar la cantidad
+            if (productoCantidadMap.containsKey(nombre)) {
+            	productoCantidadMap.put(nombre,cantidad+cantidadActual);
+                
+            }
+            else {productoCantidadMap.put(nombre,cantidadActual);
+            	
+            }
+
+            
+        }
+
+        // Imprimir los productos vencidos y la suma de cantidades
+        for (Map.Entry<String, Integer> entry : productoCantidadMap.entrySet()) {
+            String nombreProducto = entry.getKey();
+            int cantidadTotal = entry.getValue();
+
+            System.out.println(nombreProducto + ": " + cantidadTotal);
+        }
     }
+    public void retirarItemsVencidos() {
+    	for (Item i:listadoVencidos) {
+    		this.retirarItems(i, i.getCantidad());
+    		
+    	}
+    }
+    
+    
+    }
+    	
+    	
+    
  
-    }
+    
     
 
 
--- src/clases/Item.java
+++ src/clases/Item.java
@@ -1,6 +1,10 @@
 package clases;
 import java.util.List;
+
 import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 public class Item {
 	
 	    private String nombre;
@@ -8,7 +12,10 @@ public class Item {
 	    private double precio;
 	    private  String numeroSerial;
 	    private static int totalItems;
-	    private String fechaVencimiento;
+	    private Date fechaVencimiento;
+	    private static List<Item>listadoItems= new ArrayList<>();
+	    // será un listado de los productos vencidos 
+	    
 	    
 	    private Tipo tipo;
 
@@ -17,10 +24,26 @@ public class Item {
 	        this.nombre = nombre;
 	        this.cantidad = cantidad;
 	        this.precio = precio;
-	        this.fechaVencimiento=fechaVencimiento;
-	        
 	        this.tipo=tipo;
-	        totalItems++;
+	        
+	        
+	        // esto es una prueba, posiblemente lo quitaré.
+	        try {
+	            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+	            this.fechaVencimiento = formato.parse(fechaVencimiento);
+	        } catch (ParseException e) {
+	        System.err.println("Error al analizar la fecha de vencimiento: " + e.getMessage());
+	        this.fechaVencimiento = null; // Opcional: asignar un valor predeterminado en caso de error
+	        }
+	       
+	        
+	        
+	        
+	       // agregar el objeto al listado
+	        listadoItems.add(this);
+	        
+	      // actualizar la cantidad de items 
+	        totalItems+=cantidad;
 	        
 	    }
 
@@ -54,10 +77,7 @@ public class Item {
 	        this.precio = precio;
 	    }
 
-	    // Método para calcular el valor total del item
-	    public double calcularValorTotal() {
-	        return cantidad * precio;
-	    }
+	    
 
 	    @Override
 	    public String toString() {
@@ -67,13 +87,30 @@ public class Item {
 	    	return totalItems;
 	    	
 	    }
-	    public String getFechaVencimiento() {
+	    public Date getFechaVencimiento() {
 	    	return this.fechaVencimiento;
 	    }
-	    public void setFechaVencimiento(String fecha) {
+	    public void setFechaVencimiento(Date fecha) {
 	    	this.fechaVencimiento=fecha;
 	    	
 	    }
+	    // Método getter para listadoItems
+	    public static List<Item> getListadoItems() {
+	        return listadoItems;
+	    }
+
+	    // Método setter para listadoItems
+	    public static void setListadoItems(List<Item> nuevoListadoItems) {
+	        listadoItems = nuevoListadoItems;}
+	   
+	        
+	    // Método para calcular el valor total del item
+	    public double calcularValorTotal() {
+	        return cantidad * precio;
+	    }}
 	    
-	}
+	
+	    
+	    
+	
 
