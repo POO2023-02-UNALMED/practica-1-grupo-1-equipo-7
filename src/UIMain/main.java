adfc42acd33d539e52555be089439423a5f4ac54
Andres <139162301+otaku9820@users.noreply.github.com>
2023-10-18 04:37:08 -0500

    aaaaaa

---
+++ src/UIMain/main.java
@@ -0,0 +1,13 @@
+package UIMain;
+
+public class main {
+	public class MiPrograma {
+
+	    public static void main(String[] args) {
+	        System.out.printf("1.ver menú","2.registor del restaurante/n","3. inventario/n";)
+	        
+	    }
+	}
+
+
+}
