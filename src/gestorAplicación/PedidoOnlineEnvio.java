f7c753ac9315618de409f12ecfc361c09dd96d3d
Manue-droi <manusarf16@gmail.com>
2023-10-21 13:37:25 -0500

    upDate

--- src/gestorAplicación/PedidoOnlineEnvio.java
+++ src/gestorAplicación/PedidoOnlineEnvio.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class PedidoOnlineEnvio {
 	private int numeroOrden;
 	private int cantidadPlatos;
+	private String estadoPedido;
 	private String direccionPedido;
 	protected String tipoPedido = "envio";
 	private List<Plato> carrito=new ArrayList<>(); 
@@ -18,26 +19,23 @@ public class PedidoOnlineEnvio {
 		this.direccionPedido = direccionPedido;
 		this.tipoPedido = tipoPedido;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public int getNumeroOrden() {
-		return numeroOrden;
+	public String getEstadoPedido() {
+		return estadoPedido;
 	}
-	public void setNumeroOrden(int numeroOrden) {
-		this.numeroOrden = numeroOrden;
+	public void setEstadoPedido(String estadoPedido) {
+		this.estadoPedido = estadoPedido;
 	}
 	public int getCantidadPlatos() {
 		return cantidadPlatos;
 	}
 	public void setCantidadPlatos(int cantidadPlatos) {
 		this.cantidadPlatos = cantidadPlatos;
+	}
+	public int getNumeroOrden() {
+		return numeroOrden;
+	}
+	public void setNumeroOrden(int numeroOrden) {
+		this.numeroOrden = numeroOrden;
 	}
 	public String getDireccionPedido() {
 		return direccionPedido;
@@ -46,9 +44,7 @@ public class PedidoOnlineEnvio {
 		this.direccionPedido =direccionPedido;
 	}
 	public void agregarPedidoAlCarrito(Plato plato) {
-		carrito.add(plato);
-		
-		
+		carrito.add(plato);	
 	}
 	
 	public void mostarConfirmacion() {
@@ -67,5 +63,18 @@ public class PedidoOnlineEnvio {
 	}
 	
 	
-	
+	public class SistemaDeNotificacion {
+
+	    // Método para confirmar un pedido dentro de la aplicación
+	    public void confirmarPedidoEnApp(Cliente cliente, int numeroOrden) {
+	        // Realizar acciones de confirmación, como actualizar el estado del pedido
+	    	setEstadoPedido("Confirmado");
+
+	        // Enviar una notificación dentro de la aplicación al cliente
+	        String mensaje = "Su pedido con el número de orden " + getNumeroOrden() + " ha sido confirmado.";
+	        System.out.print(mensaje);
+
+	        System.out.println("Pedido confirmado en la aplicación y notificación enviada al cliente.");
+	    }
+	}
 }
